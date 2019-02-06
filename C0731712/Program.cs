0ae0c20f930274b15ff00d734cddcc16997c0999
visualstudio <visualstudio@122-PC30>
2019-02-06 11:35:37 -0500

    11:35 commit

--- C0731712/Program.cs
+++ C0731712/Program.cs
@@ -10,6 +10,61 @@ namespace C0731712
     {
         static void Main(string[] args)
         {
+            linkedlist a = new linkedlist();
+            a.run();
+            a.walkoverthelist();
+        }
+    }
+
+    class dog
+    {
+        public static int numberofdogs = 0;
+        public dog()
+        {
+            dog.numberofdogs++;
+        }
+        public string dogname;
+        public string dogbreed;
+        public dog nextdog;
+    }
+
+    class linkedlist
+    {
+        public dog current;
+        public dog head;
+        public dog peanut = new dog();
+        public dog fifi = new dog();
+        public dog jordan = new dog();
+        public dog fido = new dog();
+
+        public void run()
+        {
+            head = peanut;
+            peanut.dogname = "peanut";
+            peanut.dogbreed = "bichon";
+            peanut.nextdog = fifi;
+
+            fifi.dogname = "fifi";
+            fifi.dogbreed = "poodle";
+            fifi.nextdog = jordan;
+
+            jordan.dogname = "jordan";
+            jordan.dogbreed = "german sheppard";
+            jordan.nextdog = fido;
+
+            fido.dogname = "fido";
+            fido.dogbreed = "beagle";
+            fido.nextdog = null;
+        }
+
+        public void walkoverthelist()
+        {
+            current = head;
+            while (current!=null)
+            {
+                Console.WriteLine(current.dogname);
+                current = current.nextdog;
+            }
         }
     }
 }
